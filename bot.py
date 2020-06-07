748a0c698fede953766841d67cbda30ad7d127d5
Michael Peters <michaelpeterswa@icloud.com>
2020-06-07 15:31:18 -0700

    feat: added rich embed notifs in #issues channel

--- bot.py
+++ bot.py
@@ -4,12 +4,16 @@ import secrets
 import discord
 import asyncio
 import requests
-
+import sqlite3
+import shortuuid
 from discord.ext import commands, tasks
 
 bot = commands.Bot(command_prefix="z-", description="zombieland")
+conn = sqlite3.connect("db/issues.sqlite3")
+c = conn.cursor()
 
-DEBUG = True
+DEBUG = True  # debug printer
+# "CREATE TABLE IF NOT EXISTS issues (id TEXT, description TEXT, status INTEGER)"
 
 
 def debugPrint(text, val):
@@ -41,13 +45,22 @@ async def get_battlemetrics(server):
             await bot.change_presence(
                 activity=discord.Game(name=offline_game_name), status=discord.Status.dnd
             )
-        await asyncio.sleep(60)
+        await asyncio.sleep(60)  # run coroutine every 60 seconds
 
 
 @bot.command(pass_context=True)
-async def issue(ctx):
+async def issue(ctx, *args):
     channel = bot.get_channel(secrets.issue_channel_id)
-    await channel.send("issue created by {}".format(ctx.message.author.mention))
+    name = "submitted by {}".format(ctx.message.author)
+    description = " ".join(args)
+    id = "ID: " + shortuuid.uuid()
+
+    embed = discord.Embed(title="🛑 Issue Report 🛑", description=name, color=0xAD0303)
+    embed.add_field(name="issue", value=id, inline=False)
+    embed.add_field(name="description", value=description, inline=False)
+    embed.set_footer(text="an admin will be with you shortly.")
+    await channel.send("Attention %s: New Issue Report " % secrets.admin_role_id)
+    await channel.send(embed=embed)
 
 
 bot.run(secrets.token)
