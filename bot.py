23f6c17f3c5015312611c87df9dada5cecbfbd78
Michael Peters <michaelpeterswa@icloud.com>
2020-06-07 13:44:24 -0700

    fix: removed sqlite3 from requirements.txt as it is included in std

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+# Michael Peters
+
 import secrets
 import discord
 import asyncio
@@ -5,38 +7,47 @@ import requests
 
 from discord.ext import commands, tasks
 
-bot = commands.Bot(command_prefix='z-', description="zombieland")
+bot = commands.Bot(command_prefix="z-", description="zombieland")
 
 DEBUG = True
 
+
 def debugPrint(text, val):
-    if(val):
+    if val:
         print(text)
+
 
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    print("We have logged in as {0.user}".format(bot))
     bot.loop.create_task(get_battlemetrics(secrets.server))
+
 
 async def get_battlemetrics(server):
     while True:
-        r = requests.get("https://api.battlemetrics.com/servers/%s" %server)
+        r = requests.get("https://api.battlemetrics.com/servers/%s" % server)
         json_data = r.json()
         current_players = json_data["data"]["attributes"]["players"]
         game_name = "%s/50 players online" % current_players
         offline_game_name = "server offline"
 
-        if(json_data["data"]["attributes"]["status"] == "online"):
+        if json_data["data"]["attributes"]["status"] == "online":
             debugPrint(game_name, DEBUG)
-            await bot.change_presence(activity=discord.Game(name=game_name), status=discord.Status.dnd)
+            await bot.change_presence(
+                activity=discord.Game(name=game_name), status=discord.Status.dnd
+            )
         else:
             debugPrint(offline_game_name, DEBUG)
-            await bot.change_presence(activity=discord.Game(name=offline_game_name), status=discord.Status.dnd)
+            await bot.change_presence(
+                activity=discord.Game(name=offline_game_name), status=discord.Status.dnd
+            )
         await asyncio.sleep(60)
+
 
 @bot.command(pass_context=True)
 async def issue(ctx):
-    channel = bot.get_channel(690423117952188447)
+    channel = bot.get_channel(secrets.issue_channel_id)
     await channel.send("issue created by {}".format(ctx.message.author.mention))
+
 
 bot.run(secrets.token)
