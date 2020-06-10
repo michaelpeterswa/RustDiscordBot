b7e3be57cd9649fe643059d782b3c4efc63cdd7e
Michael Peters <michaelpeterswa@icloud.com>
2020-06-09 17:18:08 -0700

    feat: added clear, clear issue, fixed a ton of bugs

--- bot.py
+++ bot.py
@@ -34,6 +34,29 @@ async def on_ready():
     bot.loop.create_task(get_battlemetrics_server(secrets["discord"]["server"]))
 
 
+def is_me(m):
+    return m.author == bot.user
+
+
+def is_command(m):
+    return m.content.startswith("z-")
+
+
+@bot.command(pass_context=True)
+async def clearissues(ctx):
+    channel = bot.get_channel(secrets["discord"]["issue_channel_id"])
+    deleted = await channel.purge(limit=100)
+    await channel.send("❌ Deleted {} message(s)".format(len(deleted)))
+
+
+@bot.command(pass_context=True)
+async def clear(ctx):
+    channel = ctx.message.channel
+    deleted = await channel.purge(limit=100, check=is_me)
+    deleted2 = await channel.purge(limit=100, check=is_command)
+    await channel.send("❌ Deleted {} message(s)".format(len(deleted + deleted2)))
+
+
 async def get_battlemetrics_server(server):
     while True:
         r = requests.get("https://api.battlemetrics.com/servers/%s" % server)
@@ -63,40 +86,49 @@ async def get_battlemetrics_server(server):
 async def players(ctx):
     players_url = "https://api.battlemetrics.com/players?filter%5Bservers%5D=7182527&page%5Bsize%5D=50&filter%5Bonline%5D=true"
     players = requests.get(players_url)
-    players_json = players.json()
-    channel = ctx.message.channel
-    str = ""
+    if players != None:
+        players_json = players.json()
+        channel = ctx.message.channel
+        str = ""
 
-    embed = discord.Embed(title="Players online:", color=0x00A13E)
-    for player in players_json["data"]:
-        debugPrint(player["attributes"]["name"], DEBUG)
-        str = str + player["attributes"]["name"] + "\n"
+        embed = discord.Embed(title="Zombieland Rust", color=0x00A13E)
+        for player in players_json["data"]:
+            debugPrint(player["attributes"]["name"], DEBUG)
+            str = str + player["attributes"]["name"] + "\n"
 
-    embed.add_field(name="Players:", value=str, inline=True)
-    embed.set_footer(text="Thanks for playing.")
-    await channel.send(embed=embed)
+        embed.add_field(name="Players:", value=str, inline=True)
+        embed.set_footer(text="Thanks for playing.")
+        await channel.send(embed=embed)
 
 
 @bot.command(pass_context=True)
 async def issue(ctx, *args):
-    channel = bot.get_channel(secrets["discord"]["issue_channel_id"])
-    name = "submitted by {}".format(ctx.message.author)
-    description = " ".join(args)
-    id = shortuuid.uuid()
+    if not args:
+        await ctx.message.channel.send("🛑 Please include a desciption")
+    else:
+        channel = bot.get_channel(secrets["discord"]["issue_channel_id"])
+        name = "submitted by {}".format(ctx.message.author)
+        description = " ".join(args)
+        id = shortuuid.uuid()
 
-    data = (id, description, "{}".format(ctx.message.author), 0)
-    c.execute("INSERT INTO issues VALUES (?,?,?,?)", data)
+        data = (id, description, "{}".format(ctx.message.author), 0)
+        c.execute("INSERT INTO issues VALUES (?,?,?,?)", data)
 
-    embed = discord.Embed(title="🛑 Issue Report 🛑", description=name, color=0xAD0303)
-    embed.add_field(name="issue ID", value=id, inline=False)
-    embed.add_field(name="description", value=description, inline=False)
-    embed.set_footer(text="an admin will be with you shortly.")
-    conn.commit()
-    await channel.send(
-        "Attention %s: New Issue Report " % secrets["discord"]["admin_role_string"]
-    )
-    new_issue = await channel.send(embed=embed)
-    # await new_issue.add_reaction("✅")
+        embed = discord.Embed(
+            title="🛑 Issue Report 🛑", description=name, color=0xAD0303
+        )
+        embed.add_field(name="issue ID", value=id, inline=False)
+        embed.add_field(name="description", value=description, inline=False)
+        embed.set_footer(text="an admin will be with you shortly.")
+        conn.commit()
+        await ctx.message.channel.send(
+            "✅ Thanks for submitting. Check %s for more information" % channel.mention
+        )
+        await channel.send(
+            "Attention %s: New Issue Report " % secrets["discord"]["admin_role_string"]
+        )
+        new_issue = await channel.send(embed=embed)
+        # await new_issue.add_reaction("✅")
 
 
 @bot.command(pass_context=True)
@@ -126,26 +158,24 @@ async def resolve(ctx, arg1, *args):
     result = c.fetchone()
     data = (arg1,)
     if result != None:
-        if secrets["discord"]["admin_role_id"] in [y.id for y in author.roles]:
-            description = " ".join(args)
-            c.execute("UPDATE issues SET status = 1 WHERE id = (?)", data)
-            conn.commit()
+        if not args:
+            await ctx.message.channel.send("🛑 Please include a desciption")
+        else:
+            if secrets["discord"]["admin_role_id"] in [y.id for y in author.roles]:
+                description = " ".join(args)
+                c.execute("UPDATE issues SET status = 1 WHERE id = (?)", data)
+                conn.commit()
 
-            name = "resolved by {}".format(author)
-            channel = bot.get_channel(secrets["discord"]["issue_channel_id"])
-            embed = discord.Embed(
-                title="✅ Issue Resolved ✅", description=name, color=0x00A13E
-            )
-            embed.add_field(name="issue ID", value=result[0], inline=False)
-            embed.add_field(name="description", value=description, inline=False)
-            embed.set_footer(text="Thanks for your report.")
+                name = "resolved by {}".format(author)
+                channel = bot.get_channel(secrets["discord"]["issue_channel_id"])
+                embed = discord.Embed(
+                    title="✅ Issue Resolved ✅", description=name, color=0x00A13E
+                )
+                embed.add_field(name="issue ID", value=result[0], inline=False)
+                embed.add_field(name="description", value=description, inline=False)
+                embed.set_footer(text="Thanks for your report.")
 
-            await channel.send(embed=embed)
-
-
-# @bot.event
-# async def on_reaction_add(reaction, user):
-#   if reaction.emoji == '👍':
+                await channel.send(embed=embed)
 
 
 bot.run(secrets["key"]["token"])
---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t michaelpeterswa/rustserverbot .
+docker push michaelpeterswa/rustserverbot
